7b136a3f732ba45726d767f17496c7e80e6c84a6
Kevin Old <kevin@kevinold.com>
2016-07-31 22:16:09 -0500

    convert lib to ES5; get handler, index and yaml fixed for serverless v1.0

--- handler.js
+++ handler.js
@@ -1,8 +1,18 @@
 'use strict';
 
+var lib = require('./lib');
+
 // Your first function handler
 module.exports.hello = (event, context, cb) => cb(null,
   { message: 'Go Serverless v1.0! Your function executed successfully!', event }
 );
 
 // You can add more handlers here, and reference them in serverless.yaml
+module.exports.graphql = function(event, context) {
+
+  //cb(null, { message: 'TEST', event });
+
+  lib.runGraphQL(event, function (error, response) {
+    return context.done(error, response);
+  });
+};
