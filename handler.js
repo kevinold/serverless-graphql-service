4cf5092d3c0d44d919d7268655754c47cb456f6d
Kevin Old <kevin@kevinold.com>
2016-08-06 21:22:17 -0500

    clean up to work with GraphiQL app

--- handler.js
+++ handler.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var lib = require('./lib');
+var _graphql = require('graphql');
+var Schema = require('./lib/schema');
 
 // Your first function handler
 module.exports.hello = (event, context, cb) => cb(null,
@@ -8,11 +9,12 @@ module.exports.hello = (event, context, cb) => cb(null,
 );
 
 // You can add more handlers here, and reference them in serverless.yaml
-module.exports.graphql = function(event, context) {
+module.exports.graphql = function(event, context, cb) {
 
-  //cb(null, { message: 'TEST', event });
+  var query = event.body.query;
 
-  lib.runGraphQL(event, function (error, response) {
-    return context.done(error, response);
+  _graphql.graphql(Schema, query).then(function(result) {
+    //console.log('RESULT: ', result);
+    return cb(null, result);
   });
 };
