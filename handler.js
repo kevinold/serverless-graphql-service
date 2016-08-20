66cf0b49746fbe43679a77b124f70225f5b08d1f
Kevin Old <kevin@kevinold.com>
2016-08-19 22:42:21 -0500

    adjust syntax; add cors

--- handler.js
+++ handler.js
@@ -4,9 +4,9 @@ var _graphql = require('graphql');
 var Schema = require('./lib/schema');
 
 // Your first function handler
-module.exports.hello = (event, context, cb) => cb(null,
-  { message: 'Go Serverless v1.0! Your function executed successfully!', event }
-);
+module.exports.hello = function(event, context, cb) {
+  cb(null, { message: 'Hello World', event });
+}
 
 // You can add more handlers here, and reference them in serverless.yaml
 module.exports.graphql = function(event, context, cb) {
