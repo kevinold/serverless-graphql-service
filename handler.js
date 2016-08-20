79460df2cfe836043ec5470ab1a6b238efcd29d5
Kevin Old <kevin@kevinold.com>
2016-08-19 22:48:25 -0500

    remove comments

--- handler.js
+++ handler.js
@@ -3,18 +3,15 @@
 var _graphql = require('graphql');
 var Schema = require('./lib/schema');
 
-// Your first function handler
 module.exports.hello = function(event, context, cb) {
   cb(null, { message: 'Hello World', event });
 }
 
-// You can add more handlers here, and reference them in serverless.yaml
 module.exports.graphql = function(event, context, cb) {
 
   var query = event.body.query;
 
   _graphql.graphql(Schema, query).then(function(result) {
-    //console.log('RESULT: ', result);
     return cb(null, result);
   });
 };
